1d76ea1d806df7e5cb31243a34799eea98e5e407
Arkmut <xavier.dussert@gmail.com>
2018-11-19 09:42:38 +0100

    console

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -4,31 +4,20 @@ import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.tree.configuration.Algo._
 import org.apache.spark.mllib.tree.impurity.Gini
-import org.apache.spark.sql.SparkSession
 
 object App {
   def main(): Unit = {
     exo()
   }
-
   def test(): Unit = {
     println("Hello, world!")
   }
 
   def exo(): Unit = {
-    val nbIte = 10
-  }
-
-  def main(args: Array[String]) {
-
-    //Create a SparkContext to initialize Spark
-    val session = SparkSession.builder
-      .appName("SparkSample")
-      .master("spark://Spark-1.s0wsc52zzg5utp0cwcnetspzja.ax.internal.cloudapp.net:7077")
-      .getOrCreate
+   val nbIte = 10;
 
     // READ FILES
-    val sc = session.sparkContext;
+
     // Load and parse the data file
     val train = sc.textFile("data/dota2Test.csv")
     val parsedTrain = train.map { line =>
@@ -47,31 +36,30 @@ object App {
     // BUILD MODEL
     val t0 = System.nanoTime()
     var model = DecisionTree.train(parsedTrain, Classification, Gini, 20)
-    val nbIte = args.apply(0).toInt
     for(i <- 2 to nbIte) {
       model = DecisionTree.train(parsedTrain, Classification, Gini, 20)
     }
     val t1 = System.nanoTime()
     println("Time for model : " + (((t1 - t0)/1000000)/nbIte) + "ms")
 
-    /*
-        // CHECK PREDICTION
-        val labelAndPreds = parsedTrain.map { point =>
-          val prediction = model.predict(point.features)
-          (point.label, prediction)
-        }
+/*
+    // CHECK PREDICTION
+    val labelAndPreds = parsedTrain.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
 
-        val trainErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / parsedTrain.count
-        println("Training Error = " + trainErr)
-    */
+    val trainErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / parsedTrain.count
+    println("Training Error = " + trainErr)
+*/
 
     // Evaluate model on training examples and compute training error
 
     val t2 = System.nanoTime()
     var valuesAndPreds = parsedTest.map { point =>
-      val prediction = model.predict(point.features)
-      (point.label, prediction)
-    }
+        val prediction = model.predict(point.features)
+        (point.label, prediction)
+      }
     for(i <- 2 to nbIte) {
       valuesAndPreds = parsedTest.map { point =>
         val prediction = model.predict(point.features)
@@ -83,6 +71,10 @@ object App {
 
     val MSE = valuesAndPreds.map{ case(v, p) => math.pow((v - p), 2)}.mean()
     println("training Mean Squared Error = " + MSE)
-
   }
 }
+
+
+
+
+
