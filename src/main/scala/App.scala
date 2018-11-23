c8d78cfa892ece9826c59ce0a098519861dff858
Arkmut <xavier.dussert@gmail.com>
2018-11-23 15:10:17 +0100

    update scala

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -7,73 +7,100 @@ import org.apache.spark.mllib.tree.impurity.Gini
 
 object App {
   def main(): Unit = {
-    exo()
+    //exoMono(5, 1)
+    exoMulti(5)
   }
   def test(): Unit = {
     println("Hello, world!")
   }
 
-  def exo(): Unit = {
-   val nbIte = 10;
+  def exoMulti(nbIte: Int): Unit = {
+    val nbCores = Array(1,2,3,4,8,16,32)
+    val file = "dota2Train.csv"
 
-    // READ FILES
-
-    // Load and parse the data file
-    val train = sc.textFile("data/dota2Test.csv")
-    val parsedTrain = train.map { line =>
-      val parts = line.split(',').map(_.toDouble)
-      LabeledPoint(parts(0)*0.5+0.5, Vectors.dense(parts.tail).toSparse) // *0.5+0.5 to avoid negative values
-    }
-    val test = sc.textFile("data/dota2Test.csv")
-    val parsedTest = test.map { line =>
-      val parts = line.split(',').map(_.toDouble)
-      LabeledPoint(parts(0)*0.5+0.5, Vectors.dense(parts.tail).toSparse)
-    }
-
-    println("Here I am! " + parsedTrain.count())
-
-
-    // BUILD MODEL
-    val t0 = System.nanoTime()
-    var model = DecisionTree.train(parsedTrain, Classification, Gini, 20)
-    for(i <- 2 to nbIte) {
-      model = DecisionTree.train(parsedTrain, Classification, Gini, 20)
-    }
-    val t1 = System.nanoTime()
-    println("Time for model : " + (((t1 - t0)/1000000)/nbIte) + "ms")
-
-/*
-    // CHECK PREDICTION
-    val labelAndPreds = parsedTrain.map { point =>
-      val prediction = model.predict(point.features)
-      (point.label, prediction)
-    }
-
-    val trainErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / parsedTrain.count
-    println("Training Error = " + trainErr)
-*/
-
-    // Evaluate model on training examples and compute training error
-
-    val t2 = System.nanoTime()
-    var valuesAndPreds = parsedTest.map { point =>
-        val prediction = model.predict(point.features)
-        (point.label, prediction)
+    for(nbCore <- nbCores) {
+      // Load and parse the data file
+      val train = sc.textFile("data/"+file,nbCore)
+      val parsedTrain = train.map { line =>
+        val parts = line.split(',').map(_.toDouble)
+        LabeledPoint(parts(0)*0.5+0.5, Vectors.dense(parts.tail).toSparse) // *0.5+0.5 to avoid negative values
       }
-    for(i <- 2 to nbIte) {
-      valuesAndPreds = parsedTest.map { point =>
-        val prediction = model.predict(point.features)
-        (point.label, prediction)
-      }
-    }
-    val t3 = System.nanoTime()
-    println("Time for tests : " + (((t3 - t2)/1000000)/nbIte) + "ms")
 
-    val MSE = valuesAndPreds.map{ case(v, p) => math.pow((v - p), 2)}.mean()
-    println("training Mean Squared Error = " + MSE)
+      // One empty training to warn up
+      var model = DecisionTree.train(parsedTrain, Classification, Gini, 20)
+
+      // BUILD MODEL
+      val t0 = System.nanoTime()
+      for(i <- 1 to nbIte) {
+        model = DecisionTree.train(parsedTrain, Classification, Gini, 20)
+      }
+      val t1 = System.nanoTime()
+      println(nbCore + "cores : " + ((t1 - t0)/1000000/nbIte) + "ms")
+
+    }
+  }
+
+  def exoMono(nbIte: Int, nbPartitions: Int): Unit = {
+    val files = Array("dota2Train5000.csv", 
+      "dota2Train10000.csv", 
+      "dota2Train25000.csv", 
+      "dota2Train37500.csv", 
+      "dota2Train50000.csv", 
+      "dota2Train62500.csv", 
+      "dota2Train75000.csv", 
+      "dota2Train.csv");
+
+
+    println("(size data / size test), (dt learn), (dt test), (Mean Squared Error )")
+    for(file <- files){
+      // READ FILE
+      // Load and parse the data file
+      val train = sc.textFile("data/"+file,nbPartitions)
+      val parsedTrain = train.map { line =>
+        val parts = line.split(',').map(_.toDouble)
+        LabeledPoint(parts(0)*0.5+0.5, Vectors.dense(parts.tail).toSparse) // *0.5+0.5 to avoid negative values
+      }
+      val test = sc.textFile("data/dota2Test.csv")
+      val parsedTest = test.map { line =>
+        val parts = line.split(',').map(_.toDouble)
+        LabeledPoint(parts(0)*0.5+0.5, Vectors.dense(parts.tail).toSparse)
+      }
+
+      // One empty training to warn up
+      var model = DecisionTree.train(parsedTrain, Classification, Gini, 20)
+
+      // BUILD MODEL
+      val t0 = System.nanoTime()
+      for(i <- 1 to nbIte) {
+        model = DecisionTree.train(parsedTrain, Classification, Gini, 20)
+      }
+      val t1 = System.nanoTime()
+
+      // Evaluate model on training examples and compute training error
+
+      // One empty training to warn up
+      var valuesAndPreds = parsedTest.map { point =>
+          val prediction = model.predict(point.features)
+          (point.label, prediction)
+        }
+
+      val t2 = System.nanoTime()
+      for(i <- 1 to nbIte) {
+        valuesAndPreds = parsedTest.map { point =>
+          val prediction = model.predict(point.features)
+          (point.label, prediction)
+        }
+      }
+      val t3 = System.nanoTime()
+  
+      print( "(" + parsedTrain.count() + " / " + parsedTest.count() + "), ")
+      print( ((t1 - t0)/1000000/nbIte) + "ms, ")
+      print( ((t3 - t2)/1000000/nbIte) + "ms, ")
+      val MSE = valuesAndPreds.map{ case(v, p) => math.pow((v - p), 2)}.mean()
+      println(MSE)
+    }
   }
 }
-
 
 
 
